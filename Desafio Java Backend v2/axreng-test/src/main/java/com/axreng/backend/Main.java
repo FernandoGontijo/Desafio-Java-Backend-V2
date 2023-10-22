17a379227d36aed0dd72af0314f816ff55ea0d37
fernandogontijo <fernando.gontijos@hotmail.com>
2023-10-21 22:37:51 -0300

    Criando estrutura MVC

--- Desafio Java Backend v2/axreng-test/src/main/java/com/axreng/backend/Main.java
+++ Desafio Java Backend v2/axreng-test/src/main/java/com/axreng/backend/Main.java
@@ -1,12 +1,19 @@
 package com.axreng.backend;
 
 import static spark.Spark.*;
+import controller.CrawlController;
 
 public class Main {
     public static void main(String[] args) {
-        get("/crawl/:id", (req, res) ->
-                "GET /crawl/" + req.params("id"));
-        post("/crawl", (req, res) ->
-                "POST /crawl" + System.lineSeparator() + req.body());
+
+        CrawlController crawlController = new CrawlController();
+
+        get("/crawl/:id", crawlController::getCrawl);
+        post("/crawl", crawlController::postCrawl);
+
+//        get("/crawl/:id", (req, res) ->
+//                "GET /crawl/" + req.params("id"));
+//        post("/crawl", (req, res) ->
+//                "POST /crawl" + System.lineSeparator() + req.body());
     }
 }
